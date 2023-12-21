1e7e4fcc093c341f6aede19b987471e1f4d12d5f
Massimo Nocentini <massimo.nocentini@gmail.com>
2023-12-21 18:15:49 +0100

    Refine a test.

--- test/test-learning.lua
+++ test/test-learning.lua
@@ -5,6 +5,7 @@ local tests = {}
 
 function tests.test_string_byte ()
 
+    unittest.assert.equals (table.pack (string.byte ('hello', 1, 5)), {104, 101, 108, 108, 111, n = 5})
     unittest.assert.equals (table.pack (string.byte ('hello', 1, -1)), {104, 101, 108, 108, 111, n = 5})
 
 end
