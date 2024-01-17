b562dc90705a6800dd79a30da34469d0c53ff6f0
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-01-17 16:52:33 +0100

    Add result failing

--- src/unittest.lua
+++ src/unittest.lua
@@ -2,11 +2,13 @@
 local unittest = {
     metatables = {
         wasrun = {},
-        case = {}
+        case = {},
+        result = {}
     },
     traits = {
         wasrun = {},
-        case = {}
+        case = {},
+        result = {},
     },
     bootstrap = {}
 }
@@ -15,21 +17,36 @@ setmetatable (unittest.traits.wasrun, {
     __index = unittest.traits.case
 })
 
+function unittest.traits.result:started ()
+    self.runcount = self.runcount + 1
+end
+
+function unittest.traits.result:failed ()
+    self.failedcount = self.failedcount + 1
+end
+
 function unittest.traits.wasrun:logstring ()
     return table.concat (self.log, ' ')
 end
 
 function unittest.traits.case:run (client)
+    local r = unittest.bootstrap.result ()
+    r:started ()
     if client.setup then client:setup () end    
     client[self.name] (client)
-    if client.teardown then client:teardown () end   
+    if client.teardown then client:teardown () end
+    return r
 end
 
 function unittest.traits.wasrun:run (client)
     function client.test_method (recv) table.insert (self.log, 'test_method') end
     function client.setup (recv) table.insert (self.log, 'setup') end
     function client.teardown (recv) table.insert (self.log, 'teardown') end
-    getmetatable (unittest.traits.wasrun).__index.run (self, client)
+    function client.test_method_failing (recv) 
+        table.insert (self.log, 'test_method_failing')
+        error 'test_method_failing' 
+    end
+    return getmetatable (unittest.traits.wasrun).__index.run (self, client)
 end
 
 function unittest.metatables.wasrun:__index (key)
@@ -40,9 +57,30 @@ function unittest.metatables.case:__index (key)
     return unittest.traits.case[key]
 end
 
+function unittest.metatables.result:__index (key)
+    return unittest.traits.result[key]
+end
+
+function unittest.metatables.result:__tostring ()
+    return string.format ('%d ran, %d failed.', self.runcount, self.failedcount)
+end
+
 function unittest.bootstrap.wasrun ()
 
     local t = unittest.bootstrap.case 'test_method'
+
+    t.log = {}
+
+    setmetatable (t, unittest.metatables.wasrun)
+
+    return t
+
+end
+
+
+function unittest.bootstrap.wasrun_failing ()
+
+    local t = unittest.bootstrap.case 'test_method_failing'
 
     t.log = {}
 
@@ -62,5 +100,14 @@ function unittest.bootstrap.case (name)
 
 end
 
+function unittest.bootstrap.result ()
+
+    local t = { runcount = 0, failedcount = 0 }
+
+    setmetatable (t, unittest.metatables.result)
+
+    return t
+
+end
 
 return unittest
--- test/test-core.lua
+++ test/test-core.lua
@@ -9,10 +9,31 @@ end
 
 function t:test_templatemethod ()
     local wr = self.test
-
     wr:run {}
     assert (wr:logstring () == 'setup test_method teardown')
 end
 
+function t:test_result ()
+    local wr = self.test
+    local result = wr:run {}
+    assert (tostring (result) == '1 ran, 0 failed.')
+end
+
+function t:test_failed ()
+    local wr = unittest.bootstrap.wasrun_failing ()
+    local result = wr:run {}
+    assert (tostring (result) == '1 ran, 1 failed.')
+end
+
+function t:test_failed_result_formatting ()
+    local result = unittest.bootstrap.result ()
+    result:started ()
+    result:failed ()
+    assert (tostring (result) == '1 ran, 1 failed.')
+end
 
 unittest.bootstrap.case 'test_templatemethod':run (t)
+unittest.bootstrap.case 'test_result':run (t)
+unittest.bootstrap.case 'test_failed_result_formatting':run (t)
+-- unittest.bootstrap.case 'test_failed':run (t)
+
