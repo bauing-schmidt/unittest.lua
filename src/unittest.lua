1293e9c2d5d64c1ee92bfd811f5d9dbcdd8d8a00
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-01-17 11:20:03 +0100

    Restarting

--- src/unittest.lua
+++ src/unittest.lua
@@ -1,290 +1,53 @@
 
-local unittest = {}
-
-unittest.traits = {
-
-    testcase = {
-        run = function (recv, result)
-
-            if not result:started (recv) then return end
-
-            recv:setup ()
-
-            local succeed, msg = pcall (recv[recv.name], recv, result)   -- ignore returned values, just keep the succeed flag.
-
-            if not succeed then result:failed (recv, msg) end
-            
-            recv:teardown ()
-        end,
-        setup = function (recv) end,
-        teardown = function (recv) end,
+local unittest = {
+    metatables = {
+        wasrun = {},
+        case = {}
     },
-
-    testresult = {
-
-        summary = function (recv)
-            local totals = string.format ('%d run, %d failed.', recv.runcount, recv.failedcount)
-            local reasons = {}
-            for name, msg in pairs (recv.reasons) do table.insert (reasons, name .. ': ' .. msg) end
-            if #reasons > 0 then totals = totals .. '\n' .. table.concat (reasons, '\n') end
-            return totals
-        end,
-
-        started = function (recv, case)
-            if recv.seen[case.name] then return false end
-            recv.runcount = recv.runcount + 1
-            recv.seen[case.name] = true
-            return true
-        end,
-
-        failed = function (recv, case, msg) 
-            recv.failedcount = recv.failedcount + 1 
-            recv.reasons[case.name] = msg
-        end
-
+    traits = {
+        wasrun = {},
+        case = {}
     },
-
-    testcases = {
-
-        append = function (recv, test) table.insert (recv.tests, test) end,
-        run = function (recv, result)
-            for k, test in pairs (recv.tests) do
-                test:run (result)
-            end
-        end
-
-    },
-
+    bootstrap = {}
 }
 
-function unittest.case (name) 
+function unittest.traits.wasrun:test_method ()
+    self.wasrun = true
+end
+
+function unittest.traits.case:run (...)
+    self[self.name] (self, ...)
+end
+
+function unittest.metatables.wasrun:__index (key)
+    return unittest.traits.wasrun[key] or unittest.traits.case[key]
+end
+
+function unittest.metatables.case:__index (key)
+    return unittest.traits.case[key]
+end
+
+function unittest.bootstrap.wasrun (name)
+
+    local t = unittest.bootstrap.case (name, true)
+
+    t.wasrun = false
+
+    setmetatable (t, unittest.metatables.wasrun)
+
+    return t
+
+end
+
+function unittest.bootstrap.case (name, mt)
 
     local t = { name = name }
 
-    setmetatable (t, {
-
-        __index = function (recv, key) return unittest.traits.testcase[key] end
-
-    })
+    if not mt then setmetatable (t, unittest.metatables.case) end
 
     return t
 
 end
 
-function unittest.wasrun (name)
-    
-    local t = unittest.case (name)
-
-    t.log = {}
-
-    function t.test_method (recv) 
-        table.insert (recv.log, 'test_method')
-    end
-
-    function t.test_broken_method (recv) 
-        error 'explicitly raised.'
-    end
-
-    function t.logstring (recv) return table.concat (recv.log, ' ') end
-
-    function t.setup (recv)
-        table.insert (recv.log, 'setup')
-    end
-
-    function t.teardown (recv)
-        table.insert (recv.log, 'teardown')
-    end
-
-    return t
-end
-
-function unittest.new_result ()
-    local o = { 
-        runcount = 0, 
-        failedcount = 0, 
-        reasons = {},
-        seen = {},
-    }
-
-    setmetatable(o, {
-        __index = function (recv, key) return unittest.traits.testresult[key] end
-    })
-
-    return o
-end
-
-function unittest.cases ()
-
-    local o = { tests = {} }
-
-    setmetatable (o, {
-        __index = function (recv, key) return unittest.traits.testcases[key] end
-    })
-
-    return o
-
-end
-
-
-function unittest.suite (trait)
-
-
-    local function case_trait (name)
-        local c = unittest.case (name)
-
-        local __index = getmetatable (c).__index
-
-        setmetatable (c, {
-
-            __index = function (recv, key) 
-                return trait[key] or __index (recv, key) 
-            end
-
-        })
-
-        return c
-    end
-
-
-    local cases = unittest.cases ()
-
-    for name, test_f in pairs (trait) do 
-        if (type(name) == 'string' and string.sub(name, 1, 4) == 'test') then
-            cases:append (case_trait (name))
-        end
-    end
-
-    return cases
-
-end
-
-function unittest.run (tests, result)
-
-    result = result or unittest.new_result ()
-    unittest.suite (tests):run (result)
-    return result
-end
-
-function unittest.files (files)
-    return function (result)
-        result = result or unittest.new_result ()
-
-        for i, filename in ipairs (files) do
-            local spec = dofile (filename)
-            unittest.suite (spec):run (result)
-        end
-
-        return result
-    end
-end
-
-unittest.assert = {}
-
-local eq_functions = {}
-
-function eq_functions.same (a, b) return a == b end
-
-function eq_functions.equals (a, b)
-    local atype = type(a)
-    if atype == 'table' and atype == type(b) then return eq_functions.eq_tbls (a, b)
-    else return eq_functions.same (a, b) end
-end
-
-function eq_functions.eq_tbls (r, s)
-
-    local used = {}
-
-    for k, v in pairs (r) do
-
-        local missing = true
-
-        for kk, vv in pairs (s) do
-
-            if (not used[kk]) and eq_functions.equals (k, kk) then 
-
-                if eq_functions.equals (v, vv) then missing = false; used[kk] = true end
-
-            end
-
-        end
-
-        if missing then return false end
-
-    end
-
-    for k, v in pairs (s) do if not used[k] then return false end end
-
-    return true
-
-end
-
-local function tostring_recursive (obj, indent)
-
-    indent = indent or ''
-
-    if type(obj) == 'table' then
-        local s = indent
-        s = s .. '{\n'
-        for k, v in pairs (obj) do 
-            indent = indent .. '  '
-            s = s .. indent .. tostring(k) .. ': \n'
-            indent = indent .. '  '
-            s = s .. tostring_recursive (v, indent)
-        end
-        s = s .. '\n}'
-        return s
-    elseif type(obj) == 'string' then return indent .. "'" .. obj .. "'"
-    else return indent .. tostring (obj) end
-
-end
-
-function unittest.assert.same (a)
-    return function (b)
-        return assert (eq_functions.same (a, b), string.format([[
-Expected:
-%s
-Actual:
-%s]], tostring_recursive(b), tostring_recursive(a))) 
-    end
-end
-
-function unittest.assert.equals (...) 
-    local b = table.pack (...)
-    return function (...) 
-        local a = table.pack (...) 
-        return assert (eq_functions.eq_tbls (a, b),  string.format([[
-Expected:
-%s
-Actual:
-%s]], tostring_recursive(b), tostring_recursive(a))) 
-    end
-end
-
-unittest.assert.istrue = unittest.assert.equals (true)
-unittest.assert.isfalse = unittest.assert.equals (false)
-
-unittest.deny = {}
-
-function unittest.deny.same (a)
-    return function (b)
-        return assert (not eq_functions.same (a, b), string.format([[
-Expected:
-%s
-Actual:
-%s]], tostring_recursive(b), tostring_recursive(a))) 
-    end
-end
-
-function unittest.deny.equals (...) 
-    local b = table.pack (...)
-    return function (...) 
-        local a = table.pack (...) 
-        return assert (not eq_functions.eq_tbls (a, b),  string.format([[
-Expected:
-%s
-Actual:
-%s]], tostring_recursive(b), tostring_recursive(a))) 
-    end
-end
 
 return unittest
--- test/test-core.lua
+++ test/test-core.lua
@@ -1,95 +1,14 @@
 
 local unittest = require 'unittest'
 
-local tests = {}
+local t = unittest.bootstrap.case 'test_running'
 
-function tests.setup (recv) recv.result = unittest.new_result () end
-    
-function tests.test_template_method (recv)
-    local test = unittest.wasrun 'test_method'
-    test:run (recv.result)
-    unittest.assert.equals 'setup test_method teardown' (test:logstring ())
+function t:test_running ()
+    local t = unittest.bootstrap.wasrun 'test_method'
+
+    print (t.wasrun)
+    t:run ()
+    print (t.wasrun)
 end
 
-function tests.test_result (recv)
-
-    local test = unittest.wasrun 'test_method'
-    test:run (recv.result)
-    unittest.assert.equals '1 run, 0 failed.' (recv.result:summary ())
-
-end
-
-function tests.test_failed_result (recv)
-
-    local test = unittest.wasrun 'test_broken_method'
-    test:run (recv.result)
-    
-    unittest.assert.equals [[
-1 run, 1 failed.
-test_broken_method: /usr/local/share/lua/5.4/unittest.lua:85: explicitly raised.]]
-        (recv.result:summary ())
-
-end
-
-function tests.test_failedresultformatting (recv)
-
-    local not_seen = recv.result:started (recv)
-    assert (not_seen)
-
-    recv.result:failed ({name = 'test_dummy'}, 'no reason.')
-    
-    unittest.assert.equals [[
-1 run, 1 failed.
-test_dummy: no reason.]]
-        (recv.result:summary ())
-
-end
-
-function tests.test_cases (recv)
-
-    local cases = unittest.cases ()
-    cases:append (unittest.wasrun 'test_method')
-    cases:append (unittest.wasrun 'test_broken_method')
-    
-    cases:run (recv.result)
-
-    unittest.assert.equals [[
-2 run, 1 failed.
-test_broken_method: /usr/local/share/lua/5.4/unittest.lua:85: explicitly raised.]]
-        (recv.result:summary ())
-
-end
-
-function tests.test_suite (recv, result)
-
-    local suite = unittest.suite (tests)
-    suite:run (result)
-    unittest.assert.equals '9 run, 0 failed.' (result:summary ())
-
-end
-
-
-function tests.test_api_run (recv, result)
-
-    unittest.run (tests, result)
-    unittest.assert.equals '9 run, 0 failed.' (result:summary ())
-
-end
-
-
-function tests.test_api_files_assert (recv)
-    unittest.files {'test/test-assert.lua'} (recv.result)
-    print ('test/test-assert.lua: ' .. recv.result:summary ())
-    unittest.assert.equals '7 run, 0 failed.' (recv.result:summary ())
-end
-
-function tests.test_api_files_learning (recv)
-    unittest.files {'test/test-learning.lua'} (recv.result)
-    print ('test/test-learning.lua: ' .. recv.result:summary ())
-    unittest.assert.equals '3 run, 0 failed.' (recv.result:summary ())
-end
-
-local result = unittest.run (tests)
-print (result:summary ())
-
-return tests
+t:run ()
