ff13895ae2c948882ae20e42224e0ab8704282f9
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-01-19 13:11:21 +0100

    Refactoring

--- src/unittest.lua
+++ src/unittest.lua
@@ -24,9 +24,11 @@ setmetatable (unittest.traits.wasrun, {
 })
 
 function unittest.traits.result:started (name)
-    if not self.seen[name] then self.seen[name] = true
-    else self.seen[name] = false end    
-    return self.seen[name]
+    if not self.seen[name] then self.seen[name] = 0 end
+
+    self.seen[name] = self.seen[name] + 1
+
+    return self.seen[name] == 1
 end
 
 function unittest.traits.result:failed (name, error_msg)
@@ -51,30 +53,18 @@ end
 
 function unittest.traits.case:run (client, result)
     if result:started (self.name) then
-        if client.setup then client:setup () end    
-        local ok, error_msg = pcall(client[self.name], client, result)
+        if client.setup then client:setup (self) end    
+        local ok, error_msg = pcall(client[self.name], client, self, result)
         if not ok then result:failed (self.name, error_msg) end
-        if client.teardown then client:teardown () end
+        if client.teardown then client:teardown (self) end
     end
     return result
-end
-
-function unittest.traits.wasrun:run (client, result)
-    function client.test_method (recv) table.insert (self.log, 'test_method') end
-    function client.setup (recv) table.insert (self.log, 'setup') end
-    function client.teardown (recv) table.insert (self.log, 'teardown') end
-    function client.test_method_failing (recv) 
-        table.insert (self.log, 'test_method_failing')
-        error (self.error_msg)
-    end
-    return getmetatable (unittest.traits.wasrun).__index.run (self, client, result)
 end
 
 function unittest.traits.suite:insert (case) table.insert (self.cases, case) end
 
 function unittest.traits.suite:run (client, result)
     for i, case in pairs (self.cases) do case:run (client, result) end
-    return result
 end
 
 function unittest.metatables.wasrun:__index (key)
@@ -103,9 +93,9 @@ function unittest.metatables.result:__tostring ()
                           self:runcount(), fc, sep, table.concat (failure, '\n'))
 end
 
-function unittest.bootstrap.wasrun ()
+function unittest.bootstrap.wasrun (name)
 
-    local t = unittest.bootstrap.case 'test_method'
+    local t = unittest.bootstrap.case (name or 'test_method')
 
     t.log = {}
 
@@ -163,6 +153,13 @@ function unittest.bootstrap.suite (tbl)
 
     return t
 
+end
+
+function unittest.bootstrap.file (filename)
+
+    local tbl = dofile (filename)
+
+    return unittest.bootstrap.suite (tbl), tbl
 end
 
 function unittest.suite (tbl)
--- test/test-core.lua
+++ test/test-core.lua
@@ -1,88 +1,96 @@
 
 local unittest = require 'unittest'
 
-local t = {}
+local T = {
+    setup = function (self, runner) table.insert (runner.log, 'setup') end,
+    test_method = function (self, runner) table.insert (runner.log, 'test_method') end,
+    teardown = function (self, runner) table.insert (runner.log, 'teardown') end,
+    test_method_failing = function (self, runner) table.insert (runner.log, 'test_method'); error ('error_msg', 0) end,
+}
 
-local function count (t)
-    local c = 0
-    for name, _ in pairs (t) do 
-        if string.sub (name, 1, 4) == 'test' then c = c + 1 end
-    end
-    return c
+local C = {}
+
+local function count_tests (tbl)
+    local count = 0
+    for k, v in pairs (tbl) do if string.sub(k, 1, 4) == 'test' then count = count + 1 end end
+    return count
 end
 
-function t:setup ()
-    self.case = unittest.bootstrap.wasrun ()
-    self.result = unittest.bootstrap.result ()
+function C.setup (self) self.result = unittest.bootstrap.result () end
+
+function C.test_running (self)
+    
+    local runner = unittest.bootstrap.wasrun 'test_method'
+
+    runner:run (T, self.result)
+
+    assert (runner:logstring () == 'setup test_method teardown', 'wrong sequence of calls')
+    assert (tostring (self.result) == '1 ran, 0 failed.', 'wrong result string')
 end
 
-function t:test_templatemethod ()
-    local wr = self.case
-    wr:run ({}, self.result)
-    assert (wr:logstring () == 'setup test_method teardown')
-end
-
-function t:test_result ()
-    local wr = self.case
-    local result = wr:run ({}, self.result)
-    assert (tostring (result) == '1 ran, 0 failed.')
-end
-
-function t:test_failed ()
-    local wr = unittest.bootstrap.wasrun_failing 'error_msg'
-    local result = wr:run ({}, self.result)
-    assert (tostring (result) == [[
+function C.test_failing (self)
+    local runner = unittest.bootstrap.wasrun 'test_method_failing'
+    runner:run (T, self.result)
+    assert (tostring (self.result) == [[
 1 ran, 1 failed.
-test_method_failing: /usr/local/share/lua/5.4/unittest.lua:68: error_msg]])
+test_method_failing: error_msg]])
 end
 
-function t:test_failed_result_formatting ()
-    local result = self.result
-    local started = result:started ('dummy')
-    result:failed ('dummy', 'no reason')
+function C.test_failed_result_formatting (self)
+    local started = self.result:started ('dummy')
+    self.result:failed ('dummy', 'no reason')
     assert (started)
-    assert (tostring (result) == [[
+    assert (tostring (self.result) == [[
 1 ran, 1 failed.
 dummy: no reason]])
 end
 
-function t:test_suite ()
+function C.test_suite (self)
+    local result = unittest.bootstrap.result ()
     local suite = unittest.bootstrap.suite ()
-    suite:insert (unittest.bootstrap.case 'test_templatemethod')
-    suite:insert (unittest.bootstrap.case 'test_failed')
-    local result = suite:run (t, self.result)
+    suite:insert (unittest.bootstrap.case 'test_running')
+    suite:insert (unittest.bootstrap.case 'test_failing')
+    suite:run (C, result)
     assert (tostring (result) == '2 ran, 0 failed.')
 end
 
-function t:test_suite_automatically_discovered (result)
-    unittest.bootstrap.suite (t):run (t, result)
-    assert (tostring (result) == string.format('%d ran, 0 failed.', count (t)))
+function C.test_suite_automatic_discovery (self, runner, result)
+    local suite = unittest.bootstrap.suite (C)
+    suite:run (C, result)
+    assert (tostring (result) == string.format('%d ran, 0 failed.', count_tests (C)))
 end
 
-function t:test_suite_file_dummy ()
-    local filename = 'test/test-dummy.lua'
-    local tests = dofile (filename)
-    unittest.bootstrap.suite (tests):run (tests, self.result)
-    assert (tostring (self.result) == '1 ran, 0 failed.')
-    print (filename .. ': \t\t' .. tostring(self.result))
-end
 
-function t:test_suite_file_assert ()
+function C.test_file_assert (self)
     local filename = 'test/test-assert.lua'
-    local tests = dofile (filename)
-    unittest.bootstrap.suite (tests):run (tests, self.result)
-    -- assert (tostring (self.result) == string.format('%d ran, 0 failed.', count (tests)))
-    print (filename .. ': \t\t' .. tostring(self.result))
+    local suite, A = unittest.bootstrap.file (filename)
+    suite:run (A, self.result)
+    assert (tostring (self.result) == string.format('%d ran, 0 failed.', count_tests (A)))
+    print (filename .. ':\t\t' .. tostring (self.result))
 end
 
-function t:test_suite_file_learning ()
+
+function C.test_file_learning (self)
     local filename = 'test/test-learning.lua'
-    local tests = dofile (filename)
-    unittest.bootstrap.suite (tests):run (tests, self.result)
-    -- assert (tostring (self.result) == string.format('%d ran, 0 failed.', count (tests)))
-    print (filename .. ': \t' .. tostring(self.result))
+    local suite, A = unittest.bootstrap.file (filename)
+    suite:run (A, self.result)
+    assert (tostring (self.result) == string.format('%d ran, 0 failed.', count_tests (A)))
+    print (filename .. ':\t\t' .. tostring (self.result))
 end
 
-local result = unittest.suite (t)
-print ('test/test-core: \t\t' .. tostring(result))
-assert (tostring (result) == string.format('%d ran, 0 failed.', count (t)))
+function C.test_file_dummy (self)
+    local filename = 'test/test-dummy.lua'
+    local suite, A = unittest.bootstrap.file (filename)
+    suite:run (A, self.result)
+    assert (tostring (self.result) == '1 ran, 0 failed.')
+    print (filename .. ':\t\t' .. tostring (self.result))
+end
+
+local result = unittest.bootstrap.result ()
+
+unittest.bootstrap.suite (C):run (C, result)
+
+print ('test/test-core.lua:\t\t' .. tostring (result))
+
+assert (tostring (result) == string.format('%d ran, 0 failed.', count_tests (C)))
+
